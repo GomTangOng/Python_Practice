e5c152f820c61d1ede233972260aecbc73745b91
Eun Jae Kim <smrl7460@naver.com>
2017-10-10 22:29:11 +0900

    First Commit

---
+++ Python_Practice/numpy_ex01.py
@@ -0,0 +1,14 @@
+# File --> Setting --> Add Lib
+
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+
+print(x)
+print(y)
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
